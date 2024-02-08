b1cf214ab046f827c2b1d3ffbae6a60dee57270a
twinklearya <157948221+twinklearya@users.noreply.github.com>
2024-02-08 08:31:52 +0000

    change

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "twinkle@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accountId = 2 //not allowed
+
+accountEmail = "twink@gmail.com"
+accountPassword = "1234"
+accountCity = "patna"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
