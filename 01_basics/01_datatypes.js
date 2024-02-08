900ba901e460fe2bb3f7a36a1f53ff41862a1937
twinklearya <157948221+twinklearya@users.noreply.github.com>
2024-02-08 09:00:19 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version 
+
+//alert( 3 + 3 ) // we are using nodejs not browser
+
+console.log(3
+    +3) // code readability should be high
+
+console.log("twinkle")
+
+let name = "twinkle"
+let age = "24"
+let isLoggedin = false
+let state = "null"
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+
+//boolean => true or false
+//null => standalone value (represention of empty value) ; null will assign also -> not undefined
+//undefined => means not assign value 
+//symbol => uniquesness find
+
+Object
+
+console.log(typeof null); //object
+ 
+console.log(typeof undefined); //undefined
