e564badb8a67be3b502bdcfa46d267b13297e7a2
twinklearya <157948221+twinklearya@users.noreply.github.com>
2024-02-13 05:23:11 +0000

    changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -3,7 +3,7 @@ let score = 123  //
 let score = "abc123"
 let score = null // object object number o 
 let score = undefined // undefined undefined number NaN 
-let score = true // boolean boolean number 1*/
+let score = true // boolean boolean number 1
 
 let score = "twinkle" // string string number NaN 
 
@@ -32,4 +32,48 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 console.log(stringNumber);
-console.log(typeof stringNumber); // output - 33 , string 
+console.log(typeof stringNumber); // output - 33 , string */
+
+// ******************* Operations *************************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2); // 2 pe power 2
+console.log(2/2);
+console.log(2%2);*/
+
+let str1 = "hello"
+let str2 = "twink"
+
+let str3 = str1+str2
+/*console.log(str3);
+console.log("1"+2);
+console.log(1+"2"); // output - 12 
+console.log("1"+2+3);// output - 123
+console.log(3+2+"1"); //output - 51
+
+console.log((3+1)*5%3); // */
+
+//console.log(+true); // 1
+//console.log(+""); //0
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2
+
+
+//presedence operator 
+//postfix
+/*let gameCounter = 100
+gameCounter++;
+console.log(gameCounter); // output:- 101*/
+
+//prefix
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
