9cb987caf493eeed4b3ac5199349c375cee6ceb6
twinklearya <157948221+twinklearya@users.noreply.github.com>
2024-02-13 04:50:53 +0000

    changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,35 @@
-let score = 33
+/*let score = "abc" // o- string string number nan
+let score = 123  // 
+let score = "abc123"
+let score = null // object object number o 
+let score = undefined // undefined undefined number NaN 
+let score = true // boolean boolean number 1*/
+
+let score = "twinkle" // string string number NaN 
 
 console.log(typeof score);
 console.log(typeof (score));
 
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);  //NaN full form not a number
 
-let value
+
+//"33" => 33
+//"33abc" => nan
+// true => 1; false=>0;
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+//1=> true; 0=>false;
+//"" => false
+//"twinkle"=> true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber); // output - 33 , string 
