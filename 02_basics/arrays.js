415a9db37b7bc535e5159a142702dbc8c1c5509c
twinklearya <157948221+twinklearya@users.noreply.github.com>
2024-03-12 05:20:48 +0000

    changes

---
+++ 02_basics/arrays.js
@@ -0,0 +1,6 @@
+//arrays is a object in which we store collection of multitlpw items(datatypes) in single variables. 
+
+// array element cant be accesed using arbitary.
+const myarr = [0,1,2,3,4,5]
+
+console.log(myarr[3]);
